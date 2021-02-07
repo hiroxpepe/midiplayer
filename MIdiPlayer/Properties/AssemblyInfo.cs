d70a8b2ff8a7d32856ef0eb5e9e7fc5e70bea8b4
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-02-07 15:04:34 +0900

    update

--- MIdiPlayer/Properties/AssemblyInfo.cs
+++ MIdiPlayer/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MIdiPlayer")]
+[assembly: AssemblyTitle("MidiPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MIdiPlayer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("MidiPlayer")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
