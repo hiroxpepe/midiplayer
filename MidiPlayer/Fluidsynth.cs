723c0a376e04c9191b25a8b71b76100f5884f8d3
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-02-14 17:48:52 +0900

    feat: Update the FluidSynth wrapper, adding delete pointers

--- MidiPlayer/Fluidsynth.cs
+++ MidiPlayer/Fluidsynth.cs
@@ -1,13 +1,15 @@
 ﻿
+using System;
 using System.Runtime.InteropServices;
-using fluid_settings_t_ptr = System.IntPtr;
-using fluid_synth_t_ptr = System.IntPtr;
-using fluid_audio_driver_t_ptr = System.IntPtr;
-using fluid_player_t_ptr = System.IntPtr;
+using fluid_settings_t = System.IntPtr;
+using fluid_synth_t = System.IntPtr;
+using fluid_audio_driver_t = System.IntPtr;
+using fluid_player_t = System.IntPtr;
+using fluid_midi_event_t = System.IntPtr;
 
 namespace NativeFuncs {
 
-    internal static class Fluidsynth {
+    internal static unsafe class Fluidsynth {
 
         const UnmanagedType LP_Str = UnmanagedType.LPStr;
 
@@ -16,54 +18,62 @@ namespace NativeFuncs {
         internal const int FLUID_FAILED = -1;
 
         [DllImport("libfluidsynth.so")]
-        internal static extern fluid_settings_t_ptr new_fluid_settings();
+        internal static extern fluid_settings_t new_fluid_settings();
 
         [DllImport("libfluidsynth.so")]
-        internal static extern void delete_fluid_settings(fluid_settings_t_ptr settings);
+        internal static extern void delete_fluid_settings(fluid_settings_t settings);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern fluid_synth_t_ptr new_fluid_synth(fluid_settings_t_ptr settings);
+        internal static extern fluid_synth_t new_fluid_synth(fluid_settings_t settings);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern void delete_fluid_synth(fluid_synth_t_ptr synth);
+        internal static extern void delete_fluid_synth(fluid_synth_t synth);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern fluid_audio_driver_t_ptr new_fluid_audio_driver(fluid_settings_t_ptr settings, fluid_synth_t_ptr synth);
+        internal static extern fluid_audio_driver_t new_fluid_audio_driver(fluid_settings_t settings, fluid_synth_t synth);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern void delete_fluid_audio_driver(fluid_audio_driver_t_ptr driver);
+        internal static extern void delete_fluid_audio_driver(fluid_audio_driver_t driver);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_synth_sfload(fluid_synth_t_ptr synth, [MarshalAs(LP_Str)] string filename, bool reset_presets);
+        internal static extern int fluid_synth_sfload(fluid_synth_t synth, [MarshalAs(LP_Str)] string filename, bool reset_presets);
 
         [DllImport("libfluidsynth.so")]
         internal static extern int fluid_is_soundfont([MarshalAs(LP_Str)] string filename); // 1 or 0
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_synth_noteon(fluid_synth_t_ptr synth, int chan, int key, int vel);
+        internal static extern int fluid_synth_noteon(fluid_synth_t synth, int chan, int key, int vel);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_synth_noteoff(fluid_synth_t_ptr synth, int chan, int key);
+        internal static extern int fluid_synth_noteoff(fluid_synth_t synth, int chan, int key);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern fluid_player_t_ptr new_fluid_player(fluid_synth_t_ptr synth);
+        internal static extern fluid_player_t new_fluid_player(fluid_synth_t synth);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int delete_fluid_player(fluid_player_t_ptr player);
+        internal static extern int delete_fluid_player(fluid_player_t player);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_player_add(fluid_player_t_ptr player, [MarshalAs(LP_Str)] string midifile);
+        internal static extern int fluid_player_add(fluid_player_t player, [MarshalAs(LP_Str)] string midifile);
 
         [DllImport("libfluidsynth.so")]
         internal static extern int fluid_is_midifile([MarshalAs(LP_Str)] string filename); // 1 or 0
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_player_play(fluid_player_t_ptr player);
+        internal static extern int fluid_player_play(fluid_player_t player);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_player_join(fluid_player_t_ptr player);
+        internal static extern int fluid_player_join(fluid_player_t player);
 
         [DllImport("libfluidsynth.so")]
-        internal static extern int fluid_player_stop(fluid_player_t_ptr player);
+        internal static extern int fluid_player_stop(fluid_player_t player);
+
+        internal delegate int handle_midi_event_func_t(void* data, fluid_midi_event_t midi_event);
+
+        [DllImport("libfluidsynth.so")]
+        internal static extern int fluid_player_set_playback_callback(fluid_player_t player, handle_midi_event_func_t handler, void* handler_data);
+
+        [DllImport("libfluidsynth.so")]
+        internal static extern int fluid_synth_handle_midi_event(IntPtr data, fluid_midi_event_t midi_event);
     }
 }
