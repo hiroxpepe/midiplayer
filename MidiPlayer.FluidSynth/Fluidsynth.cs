6afa86c0fb50c02673f5882bedafda0839325c78
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-02-28 09:16:17 +0900

    feat: Add a Windows view as a second option for the player

--- MidiPlayer.FluidSynth/Fluidsynth.cs
+++ MidiPlayer.FluidSynth/Fluidsynth.cs
@@ -11,75 +11,81 @@ namespace NativeFuncs {
 
     internal static class Fluidsynth {
 
+        // must set to MidiPlayer.FluidSynth.csproj
+#if RUNTIME_LINUX
+        const string LIBLARY = "libfluidsynth.so";
+#elif RUNTIME_WINDOWS
+        const string LIBLARY = "libs/libfluidsynth-2.dll";
+#endif
         const UnmanagedType LP_Str = UnmanagedType.LPStr;
 
         internal const int FLUID_OK = 0;
 
         internal const int FLUID_FAILED = -1;
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern fluid_settings_t new_fluid_settings();
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern void delete_fluid_settings(fluid_settings_t settings);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern fluid_synth_t new_fluid_synth(fluid_settings_t settings);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern void delete_fluid_synth(fluid_synth_t synth);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern fluid_audio_driver_t new_fluid_audio_driver(fluid_settings_t settings, fluid_synth_t synth);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern void delete_fluid_audio_driver(fluid_audio_driver_t driver);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_synth_sfload(fluid_synth_t synth, [MarshalAs(LP_Str)] string filename, bool reset_presets);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_is_soundfont([MarshalAs(LP_Str)] string filename); // 1 or 0
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_synth_noteon(fluid_synth_t synth, int chan, int key, int vel);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_synth_noteoff(fluid_synth_t synth, int chan, int key);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern fluid_player_t new_fluid_player(fluid_synth_t synth);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int delete_fluid_player(fluid_player_t player);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_player_add(fluid_player_t player, [MarshalAs(LP_Str)] string midifile);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_is_midifile([MarshalAs(LP_Str)] string filename); // 1 or 0
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_player_play(fluid_player_t player);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_player_join(fluid_player_t player);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_player_stop(fluid_player_t player);
 
         internal delegate int handle_midi_event_func_t(void_ptr data, fluid_midi_event_t evt);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_player_set_playback_callback(fluid_player_t player, handle_midi_event_func_t handler, void_ptr handler_data);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_synth_handle_midi_event(void_ptr data, fluid_midi_event_t evt);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_synth_program_change(fluid_synth_t synth, int chan, int program);
 
-        [DllImport("libfluidsynth.so")]
+        [DllImport(LIBLARY)]
         internal static extern int fluid_synth_cc(fluid_synth_t synth, int chan, int ctrl, int val);
     }
 }
