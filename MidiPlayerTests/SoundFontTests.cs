c6b72b90b4b02a2f6e15fc2c04a9dbf2d4d3d8b3
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-03-07 18:41:33 +0900

    feat: Add a private object helper class for the test project

--- MidiPlayerTests/SoundFontTests.cs
+++ MidiPlayerTests/SoundFontTests.cs
@@ -5,13 +5,13 @@ namespace MidiPlayer.SoundFont.Tests {
     [TestClass()]
     public class SoundFontInfoTests {
         [TestMethod()]
-        public void GetInstrumentTest() {
+        public void GetVoiceTest() {
             var _target = new SoundFontInfo("../data/OmegaGMGS2.sf2");
-            var _result1 = _target.GetInstrumentName(0, 0);
+            var _result1 = _target.GetVoice(0, 0);
             Assert.AreEqual("Grand Piano", _result1); // bank:0, prog:0
-            var _result2 = _target.GetInstrumentName(8, 38);
+            var _result2 = _target.GetVoice(8, 38);
             Assert.AreEqual("Synth Bass 3", _result2); // bank:8, prog:38
-            var _result3 = _target.GetInstrumentName(128, 8);
+            var _result3 = _target.GetVoice(128, 8);
             Assert.AreEqual("Room Kit", _result3); // bank:128, prog:8
         }
     }
