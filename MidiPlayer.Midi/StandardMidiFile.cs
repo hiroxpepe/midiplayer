f79dfd91d807e97ac332f1aa258a7b243bd15fa2
Hiroyuki Adachi <hiroxpepe@gmail.com>
2022-03-04 13:44:33 +0900

    feat: #67 held the sound font file and midi file paths.

--- MidiPlayer.Midi/StandardMidiFile.cs
+++ MidiPlayer.Midi/StandardMidiFile.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,21 +23,25 @@ namespace MidiPlayer.Midi {
         // Constructor
 
         public StandardMidiFile(string target) {
-            _sequence = new Sequence();
-            _sequence.Format = 1;
-            _sequence.Load(target);
-            Map<int, (string name, int channel)> nameAndMidiChannelMap;
-            nameAndMidiChannelMap = new Map<int, (string name, int channel)>();
-            Enumerable.Range(0, _sequence.Count).ToList().ForEach(x => {
-                nameAndMidiChannelMap.Add(x, getTrackNameAndMidiChannel(x));
-            });
-            this._nameAndMidiChannelMap = new Map<int, (string name, int channel)>();
-            var idx = 0;
-            nameAndMidiChannelMap.ToList().ForEach(x => {
-                if (!x.Value.name.Equals("System Setup") && !(x.Value.name.Equals("") && x.Value.channel == -1)) { // no need track
-                    this._nameAndMidiChannelMap.Add(idx++, x.Value);
-                }
-            });
+            try {
+                _sequence = new Sequence();
+                _sequence.Format = 1;
+                _sequence.Load(target);
+                Map<int, (string name, int channel)> nameAndMidiChannelMap;
+                nameAndMidiChannelMap = new Map<int, (string name, int channel)>();
+                Enumerable.Range(0, _sequence.Count).ToList().ForEach(x => {
+                    nameAndMidiChannelMap.Add(x, getTrackNameAndMidiChannel(x));
+                });
+                this._nameAndMidiChannelMap = new Map<int, (string name, int channel)>();
+                var idx = 0;
+                nameAndMidiChannelMap.ToList().ForEach(x => {
+                    if (!x.Value.name.Equals("System Setup") && !(x.Value.name.Equals("") && x.Value.channel == -1)) { // no need track
+                        this._nameAndMidiChannelMap.Add(idx++, x.Value);
+                    }
+                });
+            } catch (Exception ex) {
+                Log.Error(ex.Message);
+            }
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
