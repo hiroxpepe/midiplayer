ac9a474de17f78f538187ed8fa99b366190fd658
Hiroyuki Adachi <hiroxpepe@gmail.com>
2022-03-12 12:30:26 +0900

    feat: #78 showed the controls for changing midi parameters.

--- MidiPlayer.Droid/MainActivity.cs
+++ MidiPlayer.Droid/MainActivity.cs
@@ -20,8 +20,8 @@ using Xamarin.Essentials;
 namespace MidiPlayer.Droid {
 
     [Activity(
-        Label = "@string/app_name", 
-        Theme = "@style/Base.Theme.MaterialComponents.Light.DarkActionBar.Bridge", 
+        Label = "@string/app_name",
+        Theme = "@style/Base.Theme.MaterialComponents.Light.DarkActionBar.Bridge",
         MainLauncher = true, 
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
         ScreenOrientation = ScreenOrientation.Portrait
